7898253ee7ca7693eac8c83dfc15566d9e6e23f4
quahho <60967744+quahho@users.noreply.github.com>
2024-04-14 22:49:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import json
 from google.cloud import bigquery
 from google.oauth2 import service_account
 # credentials = service_account.Credentials.from_service_account_file('2x-bq-key.json')
